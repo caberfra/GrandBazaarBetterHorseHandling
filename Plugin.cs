2d7e618d5526028189b9fde1cee4e3a07fce61cd
Alexus <alexusgirl12@gmail.com>
2025-09-07 21:32:22 -0400

    Initial Commit

---
+++ Plugin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using BokuMono;
+using HarmonyLib;
+
+namespace TutorialPlugin;
+
+/**
+ * The BepInPlugin attribute is used to specify the plugin's GUID, name, and version and is necessary for the plugin to be recognized by BepInEx
+ * MyPluginInfo.PLUGIN_GUID is a unique identifier for the plugin, it needs to be unique. Linked to the assembly name in the .csproj file. 
+ * MyPluginInfo.PLUGIN_NAME is the name of the plugin, it can be anything you want. Linked to the product name in the .csproj file
+ * MyPluginInfo.PLUGIN_VERSION is the version of the plugin, it can be anything you want. Linked to the version in the .csproj file
+ */
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    // This is a static reference to the logger so we can use it in other classes
+    internal static new ManualLogSource Log;
+
+    /**
+     * The Load function is called when the plugin is loaded and is where you should put your initialization code
+     * This function is called exactly once and is guaranteed to be called before any other functions in the plugin
+     * Load is the IL2CPP equivalent of Awake
+     */
+    public override void Load()
+    {
+        // Plugin startup logic
+        // Assigning the logger to the base logger(useful so you aren't calling Plugin.Log every time)
+        Log = base.Log;
+        
+        // Logs on startup to the BepInEx console and log file
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        
+        // Without this line, the patches won't be applied
+        Harmony.CreateAndPatchAll(typeof(TutorialPatch));
+    }
+    
+    private class TutorialPatch
+    {
+        
+        /**
+         * The [HarmonyPatch] attribute is used to specify the class and function to patch and is necessary for the patch to work
+         * typeof(PlayerCharacter) specifies the class we want to patch
+         * "DamageHP" specifies the function we want to patch(you can also do nameof(function) if you prefer but private functions won't work with that)
+         * new Type[] {typeof(float), typeof(bool)} specifies the parameters of the function we want to patch, this is necessary if there are multiple functions with the same name
+         * The [HarmonyPostfix] attribute is used to specify that this function should be called after the original function and is also necessary for the patch to work
+         */
+        [HarmonyPatch(typeof(PlayerCharacter))]
+        // If there is only one function with the function name(in the class), you can leave out the new Type[] part
+        [HarmonyPatch("DamageHP", new Type[] {typeof(float), typeof(bool)})] 
+        [HarmonyPostfix]
+        
+        // You can name this whatever you want, but it must be static. the parameters must match the original function, with an optional __instance parameter at the start if you want to access instance variable
+        // NOTE: not all parameters need to be included, only the ones you need
+        public static void DamageHpPatch(float damage)
+        {
+            var oldDamageValue = damage; // Store the original damage value for logging purposes
+            damage = 0; // This will make the damage taken from an action to 0
+            Log.LogInfo($"Player took {oldDamageValue} damage, but it was set to {damage} by the plugin!"); // Log to show it working
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,60 @@
+# Story of Seasons BepInEx Plugin Tutorial
+
+This example plugin demonstrates how to create a simple plugin for BepInEx IL2CPP.  
+All the plugin does is remove all stamina from player actions. The code is fully commented to help you understand how it
+works.
+
+---
+
+## Setup
+
+### Prerequisites
+
+- **.NET 6.0 SDK** or later
+- **A C# IDE** (e.g., Visual Studio, JetBrains Rider, etc.)
+- [BepInEx IL2CPP Plugin Template](https://docs.bepinex.dev/master/articles/dev_guide/plugin_tutorial/1_setup.html) (
+  optional, but highly recommended)
+- **dnSpy** (to inspect game assemblies and find methods to hook into(and see classes, enums, variables etc.))
+- **BepInEx** (installed in your game folder and run at least once, see the [BepInEx
+  documentation](https://docs.bepinex.dev/master/articles/user_guide/installation/unity_il2cpp.html) for instructions). You will need it for the decompiled DLL's.
+
+
+You can fork this repository and use it as a starting point, or create a new project with the BepInEx IL2CPP plugin
+template.
+
+I recommended to start from scratch with the BepInEx template, as it sets up the plugin info for you and helps you learn
+how to add assemblies.
+
+
+
+### Notes
+
+- **Decompiling Game Code**:  
+  The actual function code will not be readable in dnSpy due to it being compiled in il2CPP. You will need to use [Ghidra](https://github.com/NationalSecurityAgency/ghidra/releases) to decompile the code.  
+  Note that the decompiled code will be in C, so if you are unfamiliar with C, it may be hard to understand.
+  <br><br>
+- **Adding Assemblies**(this allows you to access game classes and methods):  
+  To add assemblies(only Rider and Visual Studio are covered here):
+    1. Rider - Right-click on the Dependencies option in the Explorer tab and select **"Reference... -> Add From.."**
+       and select where your games DLL's are
+    2. Visual Studio - Right-click on the Dependencies option in the Solution Explorer and select **"Add Project
+       Reference... -> Browse..."**.
+    3. Browse to the DLL you want to add (these will be in the `BepInEx/interop` folder(but I recommend moving them to another folder outside the game)).
+    4. Select the DLL and click OK. (You can check iof it was added by expanding the Dependencies and looking in assemblies(In Rider under .NET 6.0 -> Assemblies))
+<br>
+  The `.csproj` file also contains comments for clarification on what they add.
+<br><br>
+---
+## Build
+- **Building the Plugin**:  
+  To build the plugin, simply build the project in your IDE. The output DLL will be located in the `bin/Debug/net6.0` or
+  `bin/Release/net6.0` folder of your project, depending on your build configuration. 
+<br><br>You can then copy this DLL to the `BepInEx/plugins` folder of your game(The DLL will be named ex.`yourPLUGIN_GUID.DLL`).<br>Only move that DLL, nothing else.
+<br><br>There is also a post-build event in the `.csproj` file that will copy the DLL to the `BepInEx/plugins` folder of your game automatically when you build the project. You will need to change the path to your game folder in the `.csproj` file though. It is commented where that is in the file.
+
+---
+
+## Need Help?
+
+If you need assistance, join the **Story of Seasons Modding Discord**.  
+There is a tutorial available there that explains how to set up a plugin from scratch using the BepInEx IL2CPP template.
