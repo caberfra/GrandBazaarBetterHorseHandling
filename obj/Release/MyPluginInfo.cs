f1bc932a285913eab81ac44357b70dc57a47cd82
Fran Caber <25249628+caberfra@users.noreply.github.com>
2025-10-05 05:58:09 -0400

    Delete obj directory

--- obj/Release/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BetterHorseHandling
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "BetterHorseHandling";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
